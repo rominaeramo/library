a90b12fc039feb2349fec248df014a78e71853bb
rominaeramo <r.eramo@gmail.com>
2015-05-07 15:43:57 +0200

    (lezione 4) interfaccia TitleManager

--- src/it/univaq/oop/java/TitleManager.java
+++ src/it/univaq/oop/java/TitleManager.java
@@ -1,28 +1,17 @@
 package it.univaq.oop.java;
 
 import it.univaq.oop.java.business.model.Title;
-import it.univaq.oop.java.business.model.TitleKind;
 
-public class TitleManager {
+public interface TitleManager {
 	
-	private static final TitleKind BOOK = new TitleKind(1, "book");
-	private static final TitleKind MAGAZINE = new TitleKind(2, "magazine");
-	
-	private static Title[] titles = { new Title("Thinking in Java", "Pippo", "description", "872918765",
-			2006, "Paerson", BOOK)};
-	
-	public TitleManager() {
-		// TODO Auto-generated constructor stub
-	}
-	
-	//create a title
-	public static void create(Title title){
-		
-	}
-
-	public static Title[] findAllTitles() {
+	static void create(Title title) {
 		// TODO Auto-generated method stub
 		
-		return titles;
 	}
+
+	static Title[] findAllTitles() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+		
 }
